aee3c28f95c8c9b9760c86c007d3ef7a1ca677d9
Tyler <tylerwesleyquinn@gmail.com>
2020-02-19 13:39:38 -0500

    monday late commit with lecture additions and test file added, tuesday ancestor complete

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,15 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def earliest_ancestor(ancestors, starting_node, visited=None):
+    if visited is None:
+        visited = []
+
+    for i in ancestors:
+        if i[1] == starting_node:
+            visited.append(i[0])
+            return earliest_ancestor(ancestors, i[0], visited)
+
+    for i in ancestors:
+        if i[0] == starting_node and i[0] in visited:
+            return i[0]
+
+    return -1
